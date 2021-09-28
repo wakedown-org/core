34bbbc351a1db83bf417c1d40e3e802554780969
Rafael 'fumasa' Giorgetti <rafael@fumasa.org>
2021-09-27 23:12:54 -0300

    adicionando loading

--- core/world-mercator/src/WorldMercator.ts
+++ core/world-mercator/src/WorldMercator.ts
@@ -28,33 +28,22 @@ export class WorldMercator extends LitElement {
   @property({ type: Number }) height = 500;
   @property({ type: Number }) scale = 1;
   @property({ type: String }) loading = svg`
+<svg width="1280" height="800" version="1.1" id="L6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
   <rect fill="none" stroke="#000" stroke-width="4" x="25" y="25" width="50" height="50">
-    <animateTransform
-      attributeName="transform"
-      dur="0.5s"
-      from="0 50 50"
-      to="180 50 50"
-      type="rotate"
-      id="strokeBox"
-      attributeType="XML"
-      begin="rectBox.end"/>
-    </rect>
-    <rect x="27" y="27" fill="#000" width="46" height="50">
-    <animate
-      attributeName="height"
-      dur="1.3s"
-      attributeType="XML"
-      from="50" 
-      to="0"
-      id="rectBox" 
-      fill="freeze"
-      begin="0s;strokeBox.end"/>
-  </rect>`;
+    <animateTransform attributeName="transform" dur="0.5s" from="0 50 50" to="180 50 50" type="rotate" id="strokeBox" attributeType="XML" begin="rectBox.end"/>
+  </rect>
+  <rect x="27" y="27" fill="#000" width="46" height="50">
+    <animate attributeName="height" dur="1.3s" attributeType="XML" from="50" to="0" id="rectBox" fill="freeze" begin="0s;strokeBox.end"/>
+</rect>
+</svg>`;
   @property({ type: String }) layerPath = new Promise(resolve => {
     if (this._worker === null) {
       this._worker = new Worker('./dist/src/workers/worker.js', { type: 'module' });
       this._worker.onmessage = (event: any) => {
-        resolve(svg`<path d="${event.data.path}"/>`);
+        resolve(svg`
+<svg width="${this.width*this.scale}" height="${this.height*this.scale}" viewBox="0 0 ${this.width} ${this.height}">
+  <path d="${event.data.path}"/>
+</svg>`);
       }
       this._worker.postMessage({ seed: this.seed, width: this.width, height: this.height });
     }
@@ -67,9 +56,7 @@ export class WorldMercator extends LitElement {
   render() {
     return svg`
       <div class="world">
-        <svg width="${this.width*this.scale}" height="${this.height*this.scale}" viewBox="0 0 ${this.width} ${this.height}">
-          ${ until(this.layerPath, this.loading) }
-        </svg>
+        ${ until(this.layerPath, this.loading) }
       </div>
     `;
   }
